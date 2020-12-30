b805b63d696a0b2bf2db844e06e465867cf05ad9
devcrema <devcrema@gmail.com>
2020-12-30 23:05:19 +0900

    :rocket: fix init -d config bug and add deploy script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ghflow',
+    version='0.0.3',
+    author='devcrema',
+    scripts=['ghflow'],
+    python_requires='>=3.5',
+    url="https://github.com/devcrema/gitflow-for-github",
+    description="wrap github CLI for gitflow"
+)
